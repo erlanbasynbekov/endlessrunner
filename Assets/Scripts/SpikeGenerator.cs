93425ed1de92db35b014c2c34211e17fc0bc8c67
Erlan <ebasynbekov@mail.ru>
2023-10-07 00:49:23 +0600

    first commit

---
+++ Assets/Scripts/SpikeGenerator.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SpikeGenerator : MonoBehaviour
+{
+    public GameObject spike;
+    public float MinSpeed;
+    public float MaxSpeed;
+    public float currentSpeed;
+    public float SpeedMultipier;
+    void Awake()
+    {
+        currentSpeed = MinSpeed;
+        generateSpike();
+    }
+
+    public void GenerateNextSpikewihtGap()
+    {
+        float randomWait = Random.Range(0.1f, 1.2f);
+        Invoke("generateSpike", randomWait);
+    }
+    
+        
+    void generateSpike()
+    {
+        GameObject SpikeIns = Instantiate(spike, transform.position, transform.rotation);
+
+        SpikeIns.GetComponent<SpikeScript>().spikeGenerator = this;
+    }
+
+    void Update()
+    {
+        if (currentSpeed < MaxSpeed)
+        {
+            currentSpeed += SpeedMultipier;
+        }
+
+    }
+}
